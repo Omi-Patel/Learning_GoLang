7935118eed813a379bc293fe422aa1452fabe757
Om Patel <omipatel7113@gmail.com>
2025-03-11 21:56:55 +0530

    simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// number
+	fmt.Println(1)
+
+	// string
+	fmt.Println("Hello Go Lang")
+
+	// bool
+	fmt.Println(true)
+
+	// float
+	fmt.Println(10.6)
+
+	// math
+	fmt.Println(10 / 2)
+}
