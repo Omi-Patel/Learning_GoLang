1e9eb3c831d71a79449b56ee4536e548425a81b1
Om Patel <omipatel7113@gmail.com>
2025-03-11 22:09:27 +0530

    variables

---
+++ 3_variables/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+
+	// var name = "go lang"
+
+	// var isAdult = true
+
+	// var num = 18
+
+	// shorthand syntax
+
+	// name := "go language"
+
+	// in below case you need to give type to variable, bcuz you are assigning value after the initialization
+	// var name string
+	// name = "omi"
+
+	// var price float32 = 60.3
+	// var price = 90.2
+	price := 40.7
+
+	fmt.Println(price)
+}
