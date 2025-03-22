40bf65a58ef694a16d928cf921103f783dd552b0
Om Patel <omipatel7113@gmail.com>
2025-03-22 12:20:04 +0530

    clousure in go

---
+++ 14_clousure/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func counter() func() int {
+	var count int = 0
+
+	return func() int {
+		count++
+		return count
+	}
+}
+
+func main() {
+	increament := counter()
+
+	fmt.Println(increament()) // 1
+	fmt.Println(increament()) // 2
+	fmt.Println(increament()) // 3
+}
